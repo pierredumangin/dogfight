98be43bed641b7004310280f2d6a05ca1c85c9d5
pierredumangin <pierre.dumangin@viacesi.fr>
2017-06-01 15:31:08 +0200

    Phase 2 + 3

---
+++ src/jpu2016/dogfight/model/src/jpu2016/dogfight/model/IArea.java
@@ -0,0 +1,10 @@
+package jpu2016.dogfight.model;
+
+import java.awt.Image;
+
+public interface IArea {
+	/* MÉTHODES */
+	public Dimension getDimension();
+	public Image getImage();
+
+}
---
+++ src/jpu2016/dogfight/model/src/jpu2016/dogfight/model/Sky.java
@@ -0,0 +1,17 @@
+package jpu2016.dogfight.model;
+
+import java.awt.Color.*;
+
+public class Sky implements IArea {
+	/* MÉTHODES */
+	public Sky(Dimension dimension) {
+		
+	}
+	public Dimension getDimension() {
+		
+	}
+	public Image getImage() {
+		
+	}
+
+}
