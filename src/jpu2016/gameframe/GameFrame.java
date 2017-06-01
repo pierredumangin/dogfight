fb0e9f664eb6fcac4094e67eb85f39543d2ed81a
clemckr <clemckr@gmail.com>
2017-06-01 11:33:36 +0200

    Vue (Phase 2+3)

---
+++ src/jpu2016/gameframe/GameFrame.java
@@ -0,0 +1,28 @@
+package jpu2016.gameframe;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Observable;
+
+/**
+ * Created by clemckr on 01/06/2017.
+ */
+public class GameFrame extends JFrame implements KeyListener {
+// ==================== METHODES ====================
+    public GameFrame(String title, IEventPerformer performer, IGraphicsBuilder graphicsBuilder, Observable observable) {
+
+    }
+
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+}
---
+++ src/jpu2016/gameframe/IEventPerformer.java
@@ -0,0 +1,10 @@
+package jpu2016.gameframe;
+
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by clemckr on 01/06/2017.
+ */
+public interface IEventPerformer {
+    public void eventPerform(KeyEvent keyCode);
+}
